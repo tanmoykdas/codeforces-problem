2166711b5c4563f30a5b26109d7e0907e2a9668f
tanmoykdas <tnmy468@gmail.com>
2024-12-14 18:27:18 +0600

    .

--- Alice's_Adventures_in_Chess.cpp
+++ Alice's_Adventures_in_Chess.cpp
@@ -1,33 +1,60 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int t;
-    cin >> t;
-    while(t--){
-      int n, a , b;
-      cin >> n >> a >> b;
-      string s;
-      cin >> s;
-      cout << s;
-      int x = 0, y = 0;
+
+void test_case() {
+  int n, a, b;
+  cin >> n >> a >> b;
+  string s;
+  cin >> s;
+  int x = 0, y = 0;
+  int c = 0, d = 0;
+  for(int i = 0; i < s.size(); i++){
+    if(s[i] == 'N') y += 1;
+    else if(s[i] == 'E') x += 1;
+    else if(s[i] == 'W') {
+      x -= 1;
+      c++;
+    }
+    else{
+      y -= 1;
+      d++;
+    }
+    if(x == a && y == b) break;
+  } 
+  if(x == 0 && y == 0){
+    cout << "NO" << endl;
+  } else if(x == a && y == b){
+    cout << "YES" << endl;
+  } else{
+    bool flag = false;
+    while(x <= 10 + c && y <= 10 + d){
       for(int i = 0; i < s.size(); i++){
         if(s[i] == 'N') y += 1;
         else if(s[i] == 'E') x += 1;
-        else if(s[i] == 'S') y -= 1;
-        else x -= 1;
-        if(x == a && y == b) break;
+        else if(s[i] == 'W') x -= 1;
+        else y -= 1;
+        // cout << x << " " << y << endl;
+        if(x == a && y == b){
+          
+          flag = true;
+          break;
+        } 
       }
-      bool flag = false;
-      if(x == 0 && y == 0) flag = false;
-      else if(a % x == 0 && b % y == 0){
-        int aa = a / x;
-        int bb = b / y;
-        if(aa == bb) flag = true;
-      } else if(x == a && y == b) flag = true;
-      if(flag) cout << "YES" << endl;
-      else cout << "NO" << endl;
+      if(flag) break;
+      if(x < -10 || y < -10) break;
     }
-    return 0;
+    if(flag) cout << "YES" << endl;
+    else cout << "NO" << endl;
+  }
+
+}
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    test_case();
+  }
+  return 0;
 }
