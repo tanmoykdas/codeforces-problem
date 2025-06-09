a3c8ba2f16a86a724dab0ca1e0515d9983e30fff
tanmoykdas <tnmy468@gmail.com>
2025-06-10 00:28:34 +0600

    10/06/2025

--- solve/2117C.cpp
+++ solve/2117C.cpp
@@ -11,13 +11,41 @@ int main() {
     cin >> n;
     vector<int> a(n);
     for (auto& x : a) cin >> x;
-    map<int, int> mp;
-    for (int x : a) mp[x]++;
-    int mx = 0;
-    // for (auto x : a) {
-    //     if (mp[x] > mx) mx = mp[x];
-    // }
-    cout << mp[a[0]] << '\n';
+    set<int> st1, st2;
+    int c = 0;
+    st1.insert(a[0]);
+    bool first = true, second = false;
+    for (int i = 1; i < n; i++) {
+
+      if (first) {
+        st2.insert(a[i]);
+        // cout << a[i] << "f";
+        if (st1.count(a[i])) st1.erase(a[i]);
+        if (st1.size() == 0) {
+          first = false;
+          second = true;
+          c++;
+          // cout << endl;
+          continue;
+          // for (auto x : st2) cout << x << " ";
+        }
+      }
+
+      if (second) {
+        st1.insert(a[i]);
+        // cout << a[i] << "s";
+        if (st2.count(a[i])) st2.erase(a[i]);
+        if (st2.size() == 0) {
+          first = true;
+          second = false;
+          c++;
+          // for (auto x : st1) cout << x << " ";
+          // cout << endl;
+        }
+      }
+    }
+
+    cout << ++c << '\n';
   }
   return 0;
 }
