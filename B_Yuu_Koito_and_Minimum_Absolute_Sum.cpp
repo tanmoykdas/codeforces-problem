522f88a1b6cde4f9f831f22d509e466f8bcc4b44
tanmoykdas <tnmy468@gmail.com>
2025-12-07 23:40:45 +0600

    7th dec 2025

--- B_Yuu_Koito_and_Minimum_Absolute_Sum.cpp
+++ B_Yuu_Koito_and_Minimum_Absolute_Sum.cpp
@@ -4,46 +4,21 @@ void solve(){
     int n;
     cin >> n;
     int a[n];
-    for (auto& x : a) cin >> x;
-    int b = 0, c = 0, j = 0;
+    for(auto& x : a) cin >> x;
+    if (a[0] == -1 || a[n - 1] == -1) {
+        int tmp;
+        if (a[0] != -1) tmp = a[0];
+        else tmp = a[n - 1];
+        a[0] = a[n - 1] = tmp;  
+    }
     for (int i = 0; i < n; i++) {
-        if (a[i] == -1) {
-            a[i] = 0;
-            c++;
-            j = i;
-        }
+        if (a[i] == -1) a[i] = 0;
     }
-    long long sum = 0;
-    for (int i = 1; i < n; i++) {
-        sum += (a[i] - a[i - 1]);
+    cout << abs(a[n - 1] - a[0]) << endl;
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
     }
-    if (c == 0) {
-        cout << abs(sum) << endl;
-        for (auto x : a) cout << x << " ";
-        cout << endl;
-    } else if (c == 1) {
-        int optn = 0;
-        a[j] = abs(sum);
-        for (int i = 1; i < n; i++) {
-            optn += (a[i] - a[i - 1]);
-        }
-        optn = abs(optn);
-        if (optn < abs(sum)) {
-            cout << optn << endl;
-            for (auto x : a) cout << x << " ";
-            cout << endl;
-        } else {
-            a[j] = 0;
-            cout << abs(sum) << endl;
-            for (auto x : a) cout << x << " ";
-            cout << endl;
-        }
-    } else {
-        cout << 0 << endl;
-        a[j] = abs(sum);
-        for (auto x : a) cout << x << " ";
-        cout << endl;
-    }
+    cout << endl;
 }
 int main(){
     ios::sync_with_stdio(false);
