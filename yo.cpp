c4ff4dabe6fa34c976224f12cd8c44a3e1c3f073
tanmoykdas <tnmy468@gmail.com>
2025-12-26 23:20:07 +0600

    setup again sublime text

--- yo.cpp
+++ yo.cpp
@@ -1,31 +1,9 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  int n, k;
-  cin >> n >> k;
-  vector<int> a;
-  for (int i = 0; i < n; i++) {
-    int x;
-    cin >> x;
-    a.push_back(x);
-  }
-  int fi, sec;
-  if (k % 2 == 0) {
-    fi = k / 2;
-    sec = k / 2;
-    for (int i = 0 + fi; i < n - sec; i++) {
-      cout << a[i] << " ";
-    }
-  } else {
-    fi = k / 2;
-    fi += 1;
-    sec = k / 2;
-    for (int i = n - sec - 1; i >= fi + 0; i--) {
-      cout << a[i] << " ";
-    }
-  }
-  return 0;
+  int n;
+  cin >> n;
+  while (n){
+    cout << n << endl;
+  } 
 }
