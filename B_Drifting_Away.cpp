47c38a13e1e6dc250544e0394a1a545b550dc8fa
tanmoykdas <tnmy468@gmail.com>
2026-01-03 20:57:11 +0600

    7 bar chestay o wrong khailam

--- B_Drifting_Away.cpp
+++ B_Drifting_Away.cpp
@@ -3,56 +3,74 @@ using namespace std;
 void solve(){
     string s;
     cin >> s;
-    bool paise = 0, infinite = 0;
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] == '>') paise = 1;
-        if (s[i] == '<' && paise) infinite = 1;
-    }
-
-    for (int i = 0; i < s.size() - 1; i++) {
-        if (s[i] == '*' && s[i + 1] == '*') infinite = 1;
-    }
-
-    if (infinite) {
-        cout << "-1\n";
-    } else {
-        int bam_dik = 0, dan_dik = 0;
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '<' || s[i] == '*') bam_dik++;
-            else break;
-        }
-
-        for (int i = s.size() - 1; i >= 0; i--) {
-            if (s[i] == '>' || s[i] == '*') dan_dik++;
-            else break;
-        }
-        int ans = max(bam_dik, dan_dik);
-        set<char> st;
-        for (int i = 0; i < s.size(); i++) {
-            st.insert(s[i]);
-        }
-        if (st.size() == 1 && s.size() > 1 && st.count('*')) {
-            cout << "-1\n";
-            return;
-        } else if (st.size() == 1 && s.size() == 1 && st.count('*')) {
-            cout << "1\n";
-            return;
-        }
-        if (bam_dik > dan_dik) {
-            if (s[0] == '<') cout << bam_dik << "\n";
-            else cout << -1 << "\n";
-        } else if (dan_dik > bam_dik) {
-            if (s[s.size() -1] == '>') cout << dan_dik << "\n";
-            else cout << -1 << "\n";
-        }
-        else {
-            if (s[0] != '*' || s[s.size() -1] != '*') {
-                cout << ans << "\n";
-            } else {
-                cout << -1 << "\n";
+    int n = s.size();
+    int c = 0, d = 0;
+    bool paise = true;
+    if (n == 1) cout << 1 << endl;
+    else if (s[0] == '*' && s[n - 1] == '*') cout << -1 << endl;
+    else if (s[0] == '<' && s[n - 1] == '>') {
+        int suru = 0, sesh = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (s[i] == '>' && s[i + 1] == '<') {
+                paise = false;
+                break;
             }
         }
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '<') suru++;
+            else if (s[i] == '*') {
+                suru++;
+                break;
+            }
+            else break;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == '>') sesh++;
+            else if (s[i] == '*') {
+                sesh++;
+                break;
+            }
+            else break;
+        }
+        if (paise) cout << max(suru, sesh) << endl;
+        else cout << -1 << endl;
     }
+    else if (s[0] == '<') {
+        for (int i = 0; i < n - 1; i++) {
+            if ((s[i] == '<' && s[i + 1] == '<') || (s[i] == '<' && s[i + 1] == '>') || (s[i] == '<' && s[i + 1] == '*')) {
+                c++;
+            }
+            else if (s[i] == '*' && s[i + 1] == '>' && i + 1 == n - 1) {
+                c++;
+                break;
+            } else {
+                paise = false;
+                break;
+            }
+        }
+        if (s[n - 1] == '*' || s[n - 1] == '<') c++;
+        if (paise) cout << c << endl;
+        else cout << -1 << endl;
+    }
+    else if (s[n - 1] == '>') {
+        for (int i = n - 1; i > 0; i--) {
+            if ((s[i] == '>' && s[i - 1] == '>') || (s[i] == '>' && s[i - 1] == '<') || (s[i] == '>' && s[i - 1] == '*')) {
+                d++;
+            }
+            else if (s[i] == '*' && s[i - 1] == '<' && i - 1 == 0) {
+                d++;
+                break;
+            } else {
+                // cout << s[i] << " " << s[i - 1] << endl;
+                paise = false;
+                break;
+            }
+        }
+        if (s[0] == '*' || s[0] == '>') d++;
+        if (paise) cout << d << endl;
+        else cout << -1 << endl;
+    } 
+    else cout << -1 << endl;
 }
 int main(){
     ios::sync_with_stdio(false);
