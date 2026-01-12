90cf957d79a36df4074db31da504c5f8714cc0bd
tanmoykdas <tnmy468@gmail.com>
2026-01-12 19:33:35 +0600

    yesterday due to time submitted suvos solution, this is mine

--- C_MEX_rose.cpp
+++ C_MEX_rose.cpp
@@ -1,32 +1,35 @@
-#include<bits/stdc++.h>
-using ll = long long;
+#include <bits/stdc++.h>
+using namespace std;
 
-void solve(){
-    ll n, k;
-    std::cin >> n >> k;
-    std::map<ll, ll> mp;
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, k;
+    cin >> n >> k;
+    int a[n];
+    for (auto& x : a) cin >> x;
+    sort(a, a + n);
+    // for (auto x : a) cout << x << " ";
+    // cout << endl;
+    int soman = 0;
+    set<int> st;
     for (int i = 0; i < n; i++) {
-        ll a;
-        std::cin >> a;
-        mp[a]++;
+      if (a[i] > k) break;
+      else if (a[i] == k) soman++;
+      else {
+        st.insert(a[i]);
+      }
     }
-    
-    ll ans = 0;
-    for (int i = 0; i < k; i++) {
-        if (mp.find(i) == mp.end()) {
-            ans++;
-        }
+    int lagbe = k - st.size();
+    // cout << lagbe << " ";
+    if (soman) {
+      if (soman >= lagbe) cout << soman << endl;
+      else cout << lagbe << endl;
     }
-    ans = std::max(ans, mp[k]);
-    std::cout << ans << "\n";
-}
-int main(){
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-
-    int t;
-    std::cin >> t;
-    while(t--) {
-        solve();
-    }
+    else cout << lagbe << endl;
+  }
+  return 0;
 }
