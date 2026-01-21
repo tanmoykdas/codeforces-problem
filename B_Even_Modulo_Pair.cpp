816a4ab55fb1438815e5c7d63a1e9287554996f2
tanmoykdas <tnmy468@gmail.com>
2026-01-21 23:51:10 +0600

    sohoj problem chilo kintu parlam nah dite

--- B_Even_Modulo_Pair.cpp
+++ B_Even_Modulo_Pair.cpp
@@ -1,40 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  int t;
-  cin >> t;
-  while (t--) {
-    int n;
-    cin >> n;
-    int a[n];
-    for (auto& x : a) cin >> x;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        vector<int> even, odd;
+        bool flag = false;
+        int element;
 
-    bool even = true, odd = true;
-    int max_even, max_odd;
-
-    for (int i = n - 1; i >= 0; i--) {
-        if (even && a[i] % 2 == 0 && a[i] != 1) {
-            max_even = a[i];
-            even = false;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            if (a[i] % 2 == 0)
+            {
+                even.push_back(a[i]);
+            }
+            else
+            {
+                odd.push_back(a[i]);
+            }
         }
 
-        if (odd && a[i] % 2 != 0 && a[i] != 1) {
-            max_odd = a[i];
-            odd = false;
+        if (a[0] == 1 && n > 1) {
+            cout << a[0] << " " << a[n - 1] << endl;
+            continue;
         }
+
+        if (even.size() >= 2) {
+            cout << even[0] << " " << even[even.size() - 1] << endl;
+            continue;
+        }
+
+        if (odd.size() >= 2) {
+            for (int i = 1; i < odd.size(); i++) {
+                int temp = odd[i] % odd[0];
+                if (temp % 2 == 0) {
+                    element = odd[i];
+                    flag = true;
+                    break;
+                }
+            }
+            
+            if (flag) {
+                cout << odd[0] << " " << element << endl;
+                continue;
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            int temp = a[i] % a[0];
+            if (temp % 2 == 0) {
+                element = a[i];
+                flag = true;
+                break;
+            }
+        }
+
+        if (flag) {
+            cout << a[0] << " " << element << endl;
+            continue; 
+        }
+
+        cout << -1 << endl;
     }
-
-    set<int> st;
-    for (auto x : a) st.insert(x);
-
-    if (even == 0) {
-        for (int i = max_even - 2; i > 0; i -= 2) {
-            cout << i << " ";
-        }
-    }
-  }
-  return 0;
+    return 0;
 }
