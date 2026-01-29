0dc94303a2ae52bf93fc27b3325879bcbaa7bb64
tanmoykdas <tnmy468@gmail.com>
2026-01-29 16:41:31 +0600

    programming heror session cholea

--- B_Reverse_a_Permutation.cpp
+++ B_Reverse_a_Permutation.cpp
@@ -1,44 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        vector<int> a(n);
-        for (auto &x : a)
-            cin >> x;
-        int index;
-        int mx = *max_element(a.begin(), a.end());
-        for (int i = 0; i < n; i++)
-        {
-            if (a[i] == mx)
-                index = i;
-        }
-        if (a[0] != mx)
-        {
-            for (int i = index; i >= 0; i--)
-                cout << a[i] << " ";
-            for (int i = index + 1; i < n; i++)
-                cout << a[i] << " ";
-            cout << "\n";
-        } else {
-            int c = 0;
-            for (int i = 0; i < n; i++) {
-                if (a[i] == mx) {
-                    cout << a[i] << " ";
-                    ++c;
-                }
-                else break;
-            }
-            mx = *max_element(a.begin() + c, a.end());
-            cout << mx << " ";
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        b[i] = a[i];
+    }
+    sort(b.begin(), b.end());
+    reverse(b.begin(), b.end());
+    bool rev = false;
+    int index = -1;
+    for (int i = 0; i < n; i++) {
+        if (a[i] != b[i]) {
+            rev = true;
+            index = i;
+            break;
         }
     }
+    int mx = *max_element(a.begin() + index, a.end());
+    if (!rev) {
+        for (auto x : a) cout << x << " ";
+        cout << "\n";
+    } else {
+        for (int i = 0; i < index; i++) cout << a[i] << " ";
+        int pos = -1;
+        for (int i = index; i < n; i++) {
+            if (a[i] == mx) {
+                pos = i;
+                break;  
+            }
+        }
+        for (int i = pos; i >= index; i--) {
+            cout << a[i] << " ";
+        }
+        for (int i = pos + 1; i < n; i++) {
+            cout << a[i] << " ";
+        }
+        cout << "\n";
+    }
+  }
+  return 0;
 }
