942d99ca32fc780681458993b856bcc3e3959677
tanmoykdas <tnmy468@gmail.com>
2026-02-12 23:38:26 +0600

    hotash hoio nah tanmoy

--- A_Friendly_Numbers.cpp
+++ A_Friendly_Numbers.cpp
@@ -1,23 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+
+int digit_sum(ll x) {
+  int sum = 0;
+  while (x > 0) {
+    sum += x % 10;
+    x /= 10;
+  }
+  return sum;
+}
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   int t;
   cin >> t;
   while (t--) {
-    string s;
-    cin >> s;
-    if (s.size() == 1) cout << 0 << endl;
-    else {
-      int c = 0;
-      for (int i = 0; i < s.size(); i++) {
-        if (s[i] != '0') c++;
-      }
-      if (c >= 2) cout << 10 << endl;
-      else cout << 0 << endl;
+    ll n;
+    cin >> n;
+    int c = 0;
+    for (ll i = n; i <= n + 90; i++) {
+      if (i - digit_sum(i) == n) ++c;
     }
+    cout << c << endl;
   }
   return 0;
 }
