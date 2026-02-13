e6e4dccbcaf7b337547d7b80d4d6220b04e283d6
tanmoykdas <tnmy468@gmail.com>
2026-02-13 23:53:11 +0600

    tanvir er code dekhchi, mathay aschilo jodio ekbar kintu time chilo nah

---
+++ B_Another_Divisibility_Problem.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    long long n;
+    cin >> n;
+    long long y = n * 2;
+    cout << y << endl;
+  }
+  return 0;
+}
