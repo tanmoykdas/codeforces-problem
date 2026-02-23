d26aee20c50b193a3d2d1114f28a850348d309fa
tanmoykdas <tnmy468@gmail.com>
2026-02-23 22:41:30 +0600

    after solve

--- A_String_Rotation_Game.cpp
+++ A_String_Rotation_Game.cpp
@@ -1,51 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  int t;
-  cin >> t;
-  while (t--) {
-    int n; cin >> n;
-    string s; cin >> s;
-    int singles = 0;
-    if (n == 1) {
-        cout << 1 << endl;
-        continue;
-    }
-    for (int i = 0; i < n; i++) {
-        if (i == 0) {
-            if (s[i] != s[i + 1]) singles++;
-        } else if (i == n - 1) {
-            if (s[i] != s[i - 1]) singles++;
-        } else {
-            if (s[i] != s[i - 1] && s[i] != s[i + 1]) singles++;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        if (n == 1)
+            cout << 1 << endl;
+        else if (n == 2)
+        {
+            if (s[0] == s[1])
+                cout << 1 << endl;
+            else
+                cout << 2 << endl;
+        }
+        else
+        {
+            int c = 0;
+            int ind = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (s[i] == s[i + 1])
+                {
+                    ind = i + 1;
+                    break;
+                }
+            }
+            int cnt = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (s[i] != s[i + 1])
+                    cnt++;
+            }
+            if (!ind) cout << ++cnt << endl;
+                else
+                {
+                    string t = s.substr(0, ind);
+                    string u = s.substr(ind);
+                    t = u + t;
+                     c = 0;
+                    for (int i = 0; i < n - 1; i++)
+                    {
+                        if (t[i] != t[i + 1])
+                            c++;
+                    }
+                    cout << c + 1 << endl;
+                }
         }
     }
-    int insert = 0;
-    int c = 0;
-    for (int i = 0; i < n; i++) {
-        if (s[i] == s[i + 1]) c++;
-        else {
-            insert += c;
-            c = 0;
-        }
-    }
-    vector<char> v;
-    v.push_back(s[0]);
-    for (int i = 1; i < n; i++) {
-        if (s[i] != s[i - 1]) v.push_back(s[i]);
-    }
-    int ans = v.size();
-    while (singles > 0) {
-        if (insert) {
-            insert--;
-            singles--;
-            ans++;
-        } else break;
-    }
-    cout << ans << endl;
-  }
-  return 0;
+    return 0;
 }
