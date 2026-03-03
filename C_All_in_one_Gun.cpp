93483888ee084068ef971ee38c5be92be5193f70
tanmoykdas <tnmy468@gmail.com>
2026-03-03 21:40:35 +0600

    ajkei parlam nah

--- C_All_in_one_Gun.cpp
+++ C_All_in_one_Gun.cpp
@@ -1,53 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   int t;
   cin >> t;
-  while (t--) {
-    long long n, h, k;
+  while (t--)
+  {
+    int n, h, k;
     cin >> n >> h >> k;
     vector<int> a(n);
-    for (auto& x : a) cin >> x;
-    long long mx = *max_element(a.begin(), a.end());
-    bool f = true;
+    for (auto &x : a)
+      cin >> x;
+    int mx = *max_element(a.begin(), a.end());
     long long sum = 0;
-    long long ans = 0;
+    for (auto x : a)
+      sum += x;
+    
+    int x = h;
+    x -= mx;
+
+    bool f = true;
+    int sm = mx;
+    int c = 1;
     for (int i = 0; i < n; i++) {
-        sum += a[i];
-        if (sum >= h) {
-            ans = i + 1;
-            f = false;
-            break;
-        }
+      if (a[i] == mx && f) {
+        f = false;
+        continue;
+      }
+
+      if (sm >= h) break;
+      
+      if (sm += a[i] >= h) {
+        c++;
+        break;
+      } else c++;
     }
-    if (f) {
-        if (h % sum == 0) {
-            long long cycle = h / sum;
-            ans = (cycle * n) + ((cycle - 1) * k);
-            cout << ans << endl; 
-        } else {
-            long long need = h / sum;
-            sum *= need;
-            long long in = 0;
-            for (int i = 0; i < n; i++) {
-                sum += a[i];
-                if (sum >= h) {
-                    in = i + 1;
-                    break;
-                }
-            }
-            long long time = ((need * n) + in) + (need * k);
-            cout << time << endl;
-        }
-    } else {
-        long long mn = INT_MAX;
-        for (int i = 0; i <= ans; i++) {
-            if ()
-        }
-    }
+    cout << c << endl;
   }
   return 0;
 }
