87512df900aed76b62d84ee868157bd0d54fe6c6
tanmoykdas <tnmy468@gmail.com>
2026-03-04 03:26:11 +0600

    ar kono solution mathay ashe nah bara

--- C_All_in_one_Gun.cpp
+++ C_All_in_one_Gun.cpp
@@ -11,34 +11,100 @@ int main()
   {
     int n, h, k;
     cin >> n >> h >> k;
-    vector<int> a(n);
+    int a[n];
     for (auto &x : a)
       cin >> x;
-    int mx = *max_element(a.begin(), a.end());
+
     long long sum = 0;
     for (auto x : a)
       sum += x;
-    
-    int x = h;
-    x -= mx;
 
-    bool f = true;
-    int sm = mx;
-    int c = 1;
-    for (int i = 0; i < n; i++) {
-      if (a[i] == mx && f) {
-        f = false;
-        continue;
+    int mx = *max_element(a, a + n);
+    int mx_c = count(a, a + n, mx);
+
+    if (sum >= h)
+    {
+      int x = h;
+      int c = 0;
+      for (int i = 0; i < n; i++)
+      {
+        if (a[i] == mx && mx_c > 0)
+        {
+          x -= mx;
+          mx_c--;
+          c++;
+          if (x <= 0)
+            break;
+          continue;
+        }
+
+        if (x > mx)
+        {
+          x -= a[i];
+          c++;
+        }
+        else
+        {
+          if (mx_c)
+          {
+            x -= mx;
+            mx_c--;
+          }
+          else
+            x -= a[i];
+          c++;
+        }
+        if (x <= 0)
+          break;
       }
-
-      if (sm >= h) break;
-      
-      if (sm += a[i] >= h) {
-        c++;
-        break;
-      } else c++;
+      cout << c << endl;
     }
-    cout << c << endl;
+    else
+    {
+      int cycle = h / sum;
+      if (h % sum == 0)
+        cout << (n * cycle) + ((cycle - 1) * k) << endl;
+      else
+      {
+        int cycle = h / sum;
+        long long remain = h - (sum * cycle);
+
+        int x = remain;
+        int c = 0;
+        for (int i = 0; i < n; i++)
+        {
+          if (a[i] == mx && mx_c > 0)
+          {
+            x -= mx;
+            mx_c--;
+            c++;
+            if (x <= 0)
+              break;
+            continue;
+          }
+
+          if (x > mx)
+          {
+            x -= a[i];
+            c++;
+          }
+          else
+          {
+            if (mx_c)
+            {
+              x -= mx;
+              mx_c--;
+            }
+            else
+              x -= a[i];
+            c++;
+          }
+          if (x <= 0)
+            break;
+        }
+        cout << (n * cycle) + (cycle * k) + c << endl;
+      }
+    }
   }
   return 0;
 }
