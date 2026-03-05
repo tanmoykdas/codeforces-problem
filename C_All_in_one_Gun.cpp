a0da72cb91a8d902d02e8490c806da7dc4343cf7
tanmoykdas <tnmy468@gmail.com>
2026-03-05 22:44:48 +0600

    eh ar hobar nah, birokto hoye gelam

--- C_All_in_one_Gun.cpp
+++ C_All_in_one_Gun.cpp
@@ -1,110 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   int t;
   cin >> t;
-  while (t--)
-  {
+  while (t--) {
     int n, h, k;
     cin >> n >> h >> k;
-    int a[n];
-    for (auto &x : a)
-      cin >> x;
-
+    vector<int> a(n);
+    for (auto& x : a) cin >> x;
+    vector<pair<int, int>> p;
+    int mx = 0;
+    for (int i = n - 1; i >= 0; i--) {
+      mx = max(mx, a[i]);
+      p.push_back(make_pair(a[i], mx));
+    }
+    reverse(p.begin(), p.end());
     long long sum = 0;
-    for (auto x : a)
-      sum += x;
-
-    int mx = *max_element(a, a + n);
-    int mx_c = count(a, a + n, mx);
-
-    if (sum >= h)
-    {
-      int x = h;
-      int c = 0;
-      for (int i = 0; i < n; i++)
-      {
-        if (a[i] == mx && mx_c > 0)
-        {
-          x -= mx;
-          mx_c--;
+    for (auto x : a) sum += x;
+    if (h <= sum) {
+      int x = 0, c = 0;
+      for (auto i = p.begin(); i != p.end(); i++) {
+        if (x + i->second >= h) {
           c++;
-          if (x <= 0)
-            break;
-          continue;
-        }
-
-        if (x > mx)
-        {
-          x -= a[i];
-          c++;
-        }
-        else
-        {
-          if (mx_c)
-          {
-            x -= mx;
-            mx_c--;
-          }
-          else
-            x -= a[i];
-          c++;
-        }
-        if (x <= 0)
           break;
+        } else {
+          x += i->first;
+          c++;
+        }
       }
       cout << c << endl;
-    }
-    else
-    {
+    } else {
       int cycle = h / sum;
-      if (h % sum == 0)
-        cout << (n * cycle) + ((cycle - 1) * k) << endl;
-      else
-      {
-        int cycle = h / sum;
+      if (h % sum) {
         long long remain = h - (sum * cycle);
-
-        int x = remain;
+        int x = 0;
+        h = remain;
         int c = 0;
-        for (int i = 0; i < n; i++)
-        {
-          if (a[i] == mx && mx_c > 0)
-          {
-            x -= mx;
-            mx_c--;
+        
+        for (auto i = p.begin(); i != p.end(); i++) {
+          if (x + i->second >= h) {
             c++;
-            if (x <= 0)
-              break;
-            continue;
-          }
-
-          if (x > mx)
-          {
-            x -= a[i];
-            c++;
-          }
-          else
-          {
-            if (mx_c)
-            {
-              x -= mx;
-              mx_c--;
-            }
-            else
-              x -= a[i];
-            c++;
-          }
-          if (x <= 0)
             break;
+          } else {
+            x += i->first;
+            c++;
+          }
         }
         cout << (n * cycle) + (cycle * k) + c << endl;
-      }
-    }
+      } else cout << (n * cycle) + ((cycle - 1) * k) << endl;
+   
+  return 0; }
   }
-  return 0;
 }
