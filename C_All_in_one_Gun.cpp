fa1b1164130820c5c966c6f90ea1f74514cfd6fc
tanmoykdas <tnmy468@gmail.com>
2026-03-06 21:27:28 +0600

    code to eida age mone hoy click hoy retrurn 0 vul hoye gechilo

--- C_All_in_one_Gun.cpp
+++ C_All_in_one_Gun.cpp
@@ -7,51 +7,21 @@ int main() {
   int t;
   cin >> t;
   while (t--) {
-    int n, h, k;
-    cin >> n >> h >> k;
-    vector<int> a(n);
+    int n, k;
+    cin >> n >> k;
+    int a[n];
     for (auto& x : a) cin >> x;
-    vector<pair<int, int>> p;
-    int mx = 0;
-    for (int i = n - 1; i >= 0; i--) {
-      mx = max(mx, a[i]);
-      p.push_back(make_pair(a[i], mx));
-    }
-    reverse(p.begin(), p.end());
-    long long sum = 0;
-    for (auto x : a) sum += x;
-    if (h <= sum) {
-      int x = 0, c = 0;
-      for (auto i = p.begin(); i != p.end(); i++) {
-        if (x + i->second >= h) {
-          c++;
-          break;
+    sort(a, a + n);
+    int c = 1, ans = 1;
+    for (int i = 1; i < n; i++) {
+        if (a[i] - a[i - 1] <= k) {
+            ++c;
         } else {
-          x += i->first;
-          c++;
+            c = 1;
         }
-      }
-      cout << c << endl;
-    } else {
-      int cycle = h / sum;
-      if (h % sum) {
-        long long remain = h - (sum * cycle);
-        int x = 0;
-        h = remain;
-        int c = 0;
-        
-        for (auto i = p.begin(); i != p.end(); i++) {
-          if (x + i->second >= h) {
-            c++;
-            break;
-          } else {
-            x += i->first;
-            c++;
-          }
-        }
-        cout << (n * cycle) + (cycle * k) + c << endl;
-      } else cout << (n * cycle) + ((cycle - 1) * k) << endl;
-   
-  return 0; }
+        ans = max(ans, c);
+    }
+    cout << n - ans << endl;
   }
+  return 0;
 }
