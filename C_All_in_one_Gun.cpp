4cd9f7c4c96fed1c1b3ff7a940fbaa533ceae4a9
tanmoykdas <tnmy468@gmail.com>
2026-03-06 21:28:06 +0600

    oho nah, eida to all in one gun problem.. legal nesh hye geche dekhtici

--- C_All_in_one_Gun.cpp
+++ C_All_in_one_Gun.cpp
@@ -1,27 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   int t;
   cin >> t;
-  while (t--) {
-    int n, k;
-    cin >> n >> k;
-    int a[n];
-    for (auto& x : a) cin >> x;
-    sort(a, a + n);
-    int c = 1, ans = 1;
-    for (int i = 1; i < n; i++) {
-        if (a[i] - a[i - 1] <= k) {
-            ++c;
-        } else {
-            c = 1;
-        }
-        ans = max(ans, c);
+  while (t--)
+  {
+    int n, h, k;
+    cin >> n >> h >> k;
+    vector<int> a(n);
+    for (auto &x : a)
+      cin >> x;
+    vector<pair<int, int>> p;
+    int mx = 0;
+    for (int i = n - 1; i >= 0; i--)
+    {
+      mx = max(mx, a[i]);
+      p.push_back(make_pair(a[i], mx));
     }
-    cout << n - ans << endl;
+    reverse(p.begin(), p.end());
+    long long sum = 0;
+    for (auto x : a)
+      sum += x;
+    if (h <= sum)
+    {
+      int x = 0, c = 0;
+      for (auto i = p.begin(); i != p.end(); i++)
+      {
+        if (x + i->second >= h)
+        {
+          c++;
+          break;
+        }
+        else
+        {
+          x += i->first;
+          c++;
+        }
+      }
+      cout << c << endl;
+    }
+    else
+    {
+      int cycle = h / sum;
+      if (h % sum)
+      {
+        long long remain = h - (sum * cycle);
+        int x = 0;
+        h = remain;
+        int c = 0;
+
+        for (auto i = p.begin(); i != p.end(); i++)
+        {
+          if (x + i->second >= h)
+          {
+            c++;
+            break;
+          }
+          else
+          {
+            x += i->first;
+            c++;
+          }
+        }
+        cout << (n * cycle) + (cycle * k) + c << endl;
+      }
+      else
+        cout << (n * cycle) + ((cycle - 1) * k) << endl;
+    }
   }
-  return 0;
 }
