ca06f4964cd8da974145cddfc182b8ab9051bbf7
tanmoykdas <tnmy468@gmail.com>
2026-03-10 18:24:13 +0600

    Oboseshe,  tao nije sobta parie ni.. junior er code dekhe help nichi amr solution o almost kacha kachi chilo..

--- C_All_in_one_Gun.cpp
+++ C_All_in_one_Gun.cpp
@@ -1,75 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
-int main()
-{
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  int t;
+  ll t;
   cin >> t;
-  while (t--)
-  {
-    int n, h, k;
+  while (t--) {
+    ll n, h, k;
     cin >> n >> h >> k;
-    vector<int> a(n);
-    for (auto &x : a)
-      cin >> x;
-    vector<pair<int, int>> p;
-    int mx = 0;
-    for (int i = n - 1; i >= 0; i--)
-    {
-      mx = max(mx, a[i]);
-      p.push_back(make_pair(a[i], mx));
-    }
-    reverse(p.begin(), p.end());
-    long long sum = 0;
-    for (auto x : a)
-      sum += x;
-    if (h <= sum)
-    {
-      int x = 0, c = 0;
-      for (auto i = p.begin(); i != p.end(); i++)
-      {
-        if (x + i->second >= h)
-        {
-          c++;
+    ll a[n];
+    for (auto& x : a) cin >> x;
+    ll sum = 0;
+    for (auto x : a) sum += x;
+    if (sum == h) cout << n << endl;
+    else if (h % sum == 0) {
+      ll cycle = h / sum;
+      ll ans = (cycle * n) + ((cycle - 1) * k);
+      cout << ans << endl;
+    } else {
+      ll need = h / sum;
+      ll c = h % sum;
+      ll mx[n], mn[n];
+      ll temp = LLONG_MAX;
+      for (int i = 0; i < n; i++) {
+        temp = min(temp, a[i]);
+        mn[i] = temp;
+      }
+      temp = 0;
+      for (int i = n - 1; i >= 0; i--) {
+        temp = max(a[i], temp);
+        mx[i] = temp;
+      }
+      ll in = n;
+      ll sum2 = 0;
+      for (int i = 0; i < n - 1; i++) {
+        sum2 += a[i];
+        if (sum2 >= c || sum2 - mn[i] + mx[i + 1] >= c) {
+          in = i + 1;
           break;
         }
-        else
-        {
-          x += i->first;
-          c++;
-        }
       }
-      cout << c << endl;
-    }
-    else
-    {
-      int cycle = h / sum;
-      if (h % sum)
-      {
-        long long remain = h - (sum * cycle);
-        int x = 0;
-        h = remain;
-        int c = 0;
-
-        for (auto i = p.begin(); i != p.end(); i++)
-        {
-          if (x + i->second >= h)
-          {
-            c++;
-            break;
-          }
-          else
-          {
-            x += i->first;
-            c++;
-          }
-        }
-        cout << (n * cycle) + (cycle * k) + c << endl;
-      }
-      else
-        cout << (n * cycle) + ((cycle - 1) * k) << endl;
+      ll time = (need * n) + (need * k) + in;
+      cout << time << endl;
     }
   }
+  return 0;
 }
