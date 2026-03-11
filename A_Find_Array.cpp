f3ed16081e6ad9d8ed28aedeb9255b69eea56062
tanmoykdas <tnmy468@gmail.com>
2026-03-12 04:25:47 +0600

    faizlami

---
+++ A_Find_Array.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+void solve() {
+  ll n; cin >> n;
+  for (int i = 0; i < n; i++) cout << i + 2 << " ";
+  cout << endl;
+}
+
+int main() {
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+
+  ll tt;
+  cin >> tt;
+  while(tt--) solve();
+  return 0;
+}
