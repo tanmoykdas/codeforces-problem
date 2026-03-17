c11869e474474c65869793854caaa0b3f1b83b80
tanmoykdas <tnmy468@gmail.com>
2026-03-18 01:38:15 +0600

    done!

--- B_Make_it_Zigzag.cpp
+++ B_Make_it_Zigzag.cpp
@@ -9,9 +9,32 @@ void solve() {
   vector<ll> a(n);
   for (auto& x : a) cin >> x;
   ll mx = 0;
-  for (int i = 1; i < n; i += 2) mx = max(a[i], mx);
-  for (int i = 1; i < n; i += 2) a[i] = mx;
-  for (auto x : a) cout << x << " ";
+  vector<ll> b(n);
+  for (int i = 0; i < n; i++) {
+    mx = max(a[i], mx);
+    b[i] = mx;
+  }
+  ll sum = 0;
+
+  for (int i = 1; i < n; i += 2) {
+    if (b[i] > a[i]) a[i] = b[i];
+    if (i == (n - 1)) {
+      if (a[i - 1] >= a[i]) {
+        sum += (a[i - 1] - (a[i] - 1));
+        a[i - 1] = (a[i - 1] - (a[i] - 1));
+      }a[i - 1] = (a[i - 1] - (a[i] - 1));
+    } else {
+      if (a[i - 1] >= a[i]) {
+        sum += (a[i - 1] - (a[i] - 1));
+        a[i - 1] -= (a[i - 1] - (a[i] - 1));
+      }
+      if (a[i + 1] >= a[i]) {
+        sum += (a[i + 1] - (a[i] - 1));
+        a[i + 1] -= (a[i + 1] - (a[i] - 1));
+      }
+    }
+  }
+  cout << sum << endl;
 }
 
 int main() {
