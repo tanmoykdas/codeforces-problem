d35acbde5dc8d46b8297d33e89d3d4d754a3adf4
tanmoykdas <tnmy468@gmail.com>
2026-04-01 21:26:56 +0600

    April foll contest korlam, 1 tao dite pari ni 2 ta touch korchilam

---
+++ B_Are_You_Smiling.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main() {
+ios::sync_with_stdio(false);
+cin.tie(nullptr);
+
+  string s; cin >> s;
+  cout << "-U-";
+  return 0;
+}
